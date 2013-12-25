39fbeb9528c0811b16072d6a6e633d92a1e78ec6
Luiz Tsuha <luiz.tsuha@gmail.com>
2013-12-24 22:13:50 -0200

    Depot Scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 FirstApp::Application.routes.draw do
+  resources :products
+
   resources :microposts
 
   resources :users
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131219011901) do
+ActiveRecord::Schema.define(version: 20131224214219) do
 
   create_table "microposts", force: true do |t|
     t.string   "content"
     t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "products", force: true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "image_url"
+    t.decimal  "price",       precision: 8, scale: 2
     t.datetime "created_at"
     t.datetime "updated_at"
   end
